34c578a76f343a0b66e7be7dc76d852d972c4a9f
AA1 AA1 <AA1>
2024-09-19 15:17:51 +0000

    ddd

--- test11.py
+++ test11.py
@@ -14,12 +14,31 @@ def on_start(container):
 
     # call 'call_api_1' block
     call_api_1(container=container)
+    # call 'call_api_2' block
+    call_api_2(container=container)
 
     return
 
 @phantom.playbook_block()
 def call_api_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("call_api_1() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
+
+
+@phantom.playbook_block()
+def call_api_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("call_api_2() called")
 
     ################################################################################
     ## Custom Code Start
